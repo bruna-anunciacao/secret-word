10f4fda1aca3e96f7c8476dae19a563f3cb9b2d5
bruna-anunciacao <bruunaaa04@gmail.com>
2023-09-10 13:11:55 -0300

    finish styles game page

--- secretword/src/components/Game/Game.js
+++ secretword/src/components/Game/Game.js
@@ -2,9 +2,27 @@ import "./Game.css";
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Passar de fase</button>
+    <div className="game">
+        <p className="points">
+            <span>Pontuação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h2 className="tipWord">Dica: <span>Dica....</span></h2>
+        <div className="wordWrapper">
+            <span className="letter">A</span>
+            <span className="blankSpace"></span>
+        </div>
+        <div className="letterWrapper">
+            <p>Tente adivinhar uma letra na palavra:</p>
+            <form>
+                <input type="text" maxLength="1" name="letter" required/>
+                <button>Adivinhar</button>
+            </form>
+        </div>
+        <div className="wrongLettersWrapper">
+            <p>Letras já digitadas:</p>
+            <span className="wrongLetter">A</span>
+        </div>
     </div>
   )
 
